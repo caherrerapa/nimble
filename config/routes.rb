18484ebf4053b7988c5e3b10834fc9f85b01d686
Martin Turner <martin@coremelt.com>
2017-09-20 16:07:00 +0700

    added Keyword, Roo Gem, CSV file importing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :keywords do
+     collection { post :import }
+  end
+
+  root to: "keywords#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
